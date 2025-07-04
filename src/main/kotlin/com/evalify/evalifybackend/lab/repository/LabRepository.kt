739330c6e272e234c83f4f02d83ef7bd0671ca16
Renny Harlin D <95925048+RennyHarlin23@users.noreply.github.com>
2025-07-04 18:34:36 +0530

    fix: lab routes

--- src/main/kotlin/com/evalify/evalifybackend/lab/repository/LabRepository.kt
+++ src/main/kotlin/com/evalify/evalifybackend/lab/repository/LabRepository.kt
@@ -1,10 +1,22 @@
 package com.evalify.evalifybackend.lab.repository
 
 import com.evalify.evalifybackend.lab.domain.Lab
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
 import java.util.UUID
 
 @RepositoryRestResource(path = "lab")
-interface LabRepository : JpaRepository<Lab,UUID> {
+interface LabRepository : JpaRepository<Lab, UUID> {
+
+    @Query("SELECT l FROM Lab l WHERE LOWER(l.name) LIKE LOWER(CONCAT('%', :query, '%')) OR LOWER(l.block) LIKE LOWER(CONCAT('%', :query, '%')) OR LOWER(l.ipSubnet) LIKE LOWER(CONCAT('%', :query, '%'))")
+    fun findByNameOrBlockOrIpSubnetContainingIgnoreCase(@Param("query") query: String): List<Lab>
+
+    @Query("SELECT l FROM Lab l WHERE LOWER(l.name) LIKE LOWER(CONCAT('%', :query, '%')) OR LOWER(l.block) LIKE LOWER(CONCAT('%', :query, '%')) OR LOWER(l.ipSubnet) LIKE LOWER(CONCAT('%', :query, '%'))")
+    fun findByNameOrBlockOrIpSubnetContainingIgnoreCasePage(@Param("query") query: String, pageable: Pageable): Page<Lab>
+
+    override fun findAll(pageable: Pageable): Page<Lab>
 }
