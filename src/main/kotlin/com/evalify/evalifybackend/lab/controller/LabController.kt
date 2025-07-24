efbc8fdc5a12d4b4fb210cad0188a4c7877338fe
Sharvesh Sivagnanam <sharveshprojects@gmail.com>
2025-07-24 15:02:39 +0530

    fix: changed route name for labs

--- src/main/kotlin/com/evalify/evalifybackend/lab/controller/LabController.kt
+++ src/main/kotlin/com/evalify/evalifybackend/lab/controller/LabController.kt
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*
 import java.util.UUID
 
 @RestController
-@RequestMapping("/lab")
+@RequestMapping("/api/lab")
 class LabController(
     private val labService: LabService
 ) {
